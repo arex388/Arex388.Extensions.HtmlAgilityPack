d247a6f204ac361243b1ed458e8cad5aaf70dc20
arex388 <31530399+arex388@users.noreply.github.com>
2020-08-23 12:15:47 -0700

    Initial commit

---
+++ HtmlNodeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HtmlAgilityPack {
+    public static class HtmlNodeExtensions {
+        private const string CommentNode = "//comment()";
+        private const string DocTypeNode = "<!DOCTYPE html>";
+        private const string TextNode = "//text()";
+
+        public static IList<HtmlNode> SelectNodesAsList(
+            this HtmlNode node,
+            string xpath) => node.SelectNodes(xpath)?.ToList() ?? new List<HtmlNode>(0);
+
+        public static void TrimWhitespace(
+            this HtmlNode document) {
+            var textNodes = document.SelectNodesAsList(TextNode).Where(
+                n => string.IsNullOrWhiteSpace(n.InnerHtml));
+
+            foreach (var textNode in textNodes) {
+                textNode.Remove();
+            }
+
+            var commentNodes = document.SelectNodesAsList(CommentNode).Where(
+                n => n.InnerHtml != DocTypeNode);
+
+            foreach (var commentNode in commentNodes) {
+                commentNode.Remove();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Arex388.Extensions.HtmlAgilityPack
+
+A very small extension to [HtmlAgilityPack](https://html-agility-pack.net/) to trim whitespace and minify HTML.
