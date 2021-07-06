db7f6ffbf26483badc54c80bb206c0ddf61ce9b5
arex388 <31530399+arex388@users.noreply.github.com>
2021-07-05 17:49:02 -0700

    Updated dependencies, cleaned up code.

--- HtmlNodeExtensions.cs
+++ HtmlNodeExtensions.cs
@@ -3,25 +3,21 @@ using System.Linq;
 
 namespace HtmlAgilityPack {
     public static class HtmlNodeExtensions {
-        private const string CommentNode = "//comment()";
-        private const string DocTypeNode = "<!DOCTYPE html>";
-        private const string TextNode = "//text()";
-
         public static IList<HtmlNode> SelectNodesAsList(
             this HtmlNode node,
             string xpath) => node.SelectNodes(xpath)?.ToList() ?? new List<HtmlNode>(0);
 
         public static void TrimWhitespace(
             this HtmlNode document) {
-            var textNodes = document.SelectNodesAsList(TextNode).Where(
+            var textNodes = document.SelectNodesAsList("//text()").Where(
                 n => string.IsNullOrWhiteSpace(n.InnerHtml));
 
             foreach (var textNode in textNodes) {
                 textNode.Remove();
             }
 
-            var commentNodes = document.SelectNodesAsList(CommentNode).Where(
-                n => n.InnerHtml != DocTypeNode);
+            var commentNodes = document.SelectNodesAsList("//comment()").Where(
+                n => n.InnerHtml != "<!DOCTYPE html>");
 
             foreach (var commentNode in commentNodes) {
                 commentNode.Remove();
